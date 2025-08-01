bf9bb2a3d5eb957389df14af15436c1c45e7e285
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-08-01 15:02:17 +0300

    Update TF

--- main.tf
+++ main.tf
@@ -45,16 +45,16 @@ resource "azurerm_linux_web_app" "alwa" {
   }
 
   connection_string {
-    name = "DefaultConnection"
-    type = "SQLAzure"
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
     value = "Data Source=tcp:${azurerm_mssql_server.server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.database.name};User ID=${azurerm_mssql_server.server.administrator_login};Password=${azurerm_mssql_server.server.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
   }
 }
 
 resource "azurerm_app_service_source_control" "aassc" {
-  app_id                 = azurerm_linux_web_app.alwa.id
-  repo_url               = "https://github.com/StiliyanEnchev/Azure-Web-App-with-Database-TaskBoard"
-  branch                 = "main"
+  app_id   = azurerm_linux_web_app.alwa.id
+  repo_url = "https://github.com/StiliyanEnchev/Azure-Web-App-with-Database-TaskBoard"
+  branch   = "main"
 }
 
 resource "azurerm_mssql_server" "server" {
@@ -67,12 +67,12 @@ resource "azurerm_mssql_server" "server" {
 }
 
 resource "azurerm_mssql_database" "database" {
-  name         = "TaskBoardDB${random_integer.ri.result}"
-  server_id    = azurerm_mssql_server.server.id
-  collation    = "SQL_Latin1_General_CP1_CI_AS"
-  license_type = "LicenseIncluded"
-  max_size_gb  = 2
-  sku_name     = "S0"
+  name           = "TaskBoardDB${random_integer.ri.result}"
+  server_id      = azurerm_mssql_server.server.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 2
+  sku_name       = "S0"
   zone_redundant = false
 }
 
